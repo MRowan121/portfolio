2608d394949e04bf9277ab7fbf25adce78d84bf9
Matt Rowan <matt.rowan121@gmail.com>
2023-10-11 15:51:14 -0400

    Add conditional rendering of home & message icons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
-import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillMessage,
+  AiFillHome,
+} from "react-icons/ai";
 import resume from "../Utilities/M-Rowan-Resume.pdf";
+import { Link, useLocation } from "react-router-dom";
 
 const Nav = ({ darkMode, toggleDarkMode }) => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  const isContactPage = location.pathname.includes("/contact");
+
   return (
     <nav className="pt-10 pb-3 flex justify-between sticky top-0 bg-white dark:bg-gray-900">
       <div className="text-3xl flex justify-center gap-6 py-3  dark:text-white sm:text-4xl">
@@ -17,6 +27,16 @@ const Nav = ({ darkMode, toggleDarkMode }) => {
         <a href="https://github.com/MRowan121" target="_blank" rel="noreferrer">
           <AiFillGithub className="icon" />
         </a>
+        {isContactPage ? null : (
+          <Link to="/contact">
+            <AiFillMessage className="icon" />
+          </Link>
+        )}
+        {isHomePage ? null : (
+          <Link to="/">
+            <AiFillHome className="icon" />
+          </Link>
+        )}
       </div>
       <ul className="flex items-center">
         <li>
